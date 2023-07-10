0da0a8f582e7a648f6dffac3aabd102d92c23a03
pdilip84 <parmardilip2013@gmail.com>
2023-07-10 18:50:41 +0530

    Added index5.php with deleting user

---
+++ index5.php
@@ -0,0 +1,238 @@
+<?php
+echo '<pre>';
+// print_r($_SERVER);
+
+/*Connect with database using PDO class */
+$dbhostname = 'localhost';
+$dbusername = 'root';
+$dbdatabase = 'githubform';
+$dbpass = '';
+
+$successMsg = '';
+$errorAry = array();
+
+$dsn = "mysql:hostname=" . $dbhostname . ";dbname=" . $dbdatabase;
+$pdo = new pdo($dsn, $dbusername, $dbpass);
+
+if (isset($_REQUEST['edituser']) && !empty($_REQUEST['edituser'])) {
+    $edituser =  $_REQUEST['edituser'];
+    // echo $edituser;
+    editUser($edituser);
+}
+if (isset($_REQUEST['deleteuser']) && !empty($_REQUEST['deleteuser'])) {
+    $deleteuser =  $_REQUEST['deleteuser'];
+    // echo $edituser;
+    $successMsg = deleteUser($deleteuser, $pdo);
+}
+/* We are creating function here to get all data because we need to run this function on page load as well as when we submit the new data at that time we need to call it again so we can see newly added data into table */
+function showAllData($pdo)
+{
+    return $resultall = $pdo->query("select * from users");
+}
+$resultall = showAllData($pdo);
+// var_dump($result);
+// echo $resultall->rowCount();
+$resultall->setFetchMode(PDO::FETCH_ASSOC);
+
+function editUser($id)
+{
+    echo $id;
+}
+function deleteUser($id, $pdo)
+{
+    // "DELETE FROM users WHERE `users`.`id` = 27"
+    $stmt = $pdo->prepare("DELETE FROM users WHERE `users`.`id` = :id");
+    // $pdo->bindValue(':id', $id);
+    $stmt->bindValue(':id', $id);
+    $result = $stmt->execute();
+    if (isset($result) && $result > 0) {
+        return 'Data Deleted successfully';
+    }
+}
+
+/* When form is submitting */
+if (isset($_POST['submitformbtn'])) {
+    /* Define error array as empty array */
+
+    // print_r($_POST);
+
+    /* Check if someone skip front end js validation and reach to server, just check empty values and throw back the errors in array */
+    if (isset($_POST['username']) && empty($_POST['username'])) {
+        array_push($errorAry, 'Name must be provided');
+        $errorUsername = 'Name must be provided';
+    }
+    if (isset($_POST['useremail']) && empty($_POST['useremail'])) {
+        array_push($errorAry, 'Email must be provided');
+    }
+    if (isset($_POST['userpassword']) && empty($_POST['userpassword'])) {
+        array_push($errorAry, 'Password must be provided');
+    }
+    if ($_POST['userpassword'] !== $_POST['confirmpassword']) {
+        array_push($errorAry, 'Password does not match');
+    }
+
+    // print_r($errorAry);
+    // echo count($errorAry);
+
+    /* If error array has values, means server side validation fail */
+    if (count($errorAry) > 0) {
+        /* Convert array to string to display in front form */
+        $errorStr = '';
+        foreach ($errorAry as $key => $value) {
+            $errorStr = $errorStr . $value . "<br>";
+        }
+        // echo $errorStr;
+    }
+    /* Server side valiation passes */ else {
+        /* sanatize variable */
+        $username = filter_var(htmlspecialchars(trim($_POST['username'])), FILTER_SANITIZE_SPECIAL_CHARS);
+
+        $useremail = filter_var(htmlspecialchars(trim($_POST['useremail'])), FILTER_SANITIZE_EMAIL);
+
+        $userpassword = filter_var(htmlspecialchars(trim($_POST['userpassword'])), FILTER_SANITIZE_SPECIAL_CHARS);
+
+        // echo $username;
+        // echo $useremail;
+
+        /* Do password Hashing */
+        $userpassword = password_hash($userpassword, PASSWORD_DEFAULT);
+
+        // echo $userpassword;
+        // echo password_verify($userpassword, '$2y$10$J9YPmAqYp.7yqfGIEhhEtuKDEEEyH01ARWoB0yScLEM6eAmc11OhW');
+
+
+        // $stmt = $pdo->query("INSERT INTO `users` (`id`, `username`, `useremail`, `userpass`, `inserdata`) VALUES (NULL, 'pdilip84', 'demo@demo.com', 'superman', '2023-07-07 17:02:27')");
+
+        /* prepare, bind & execute OR you can direcly do $pdo->query  but that is not safe */
+
+        $stmt = $pdo->prepare("INSERT INTO `users` (`id`, `username`, `useremail`, `userpass`, `inserdata`) VALUES (NULL, :username, :useremail, :userpass, now())");
+        $stmt->bindValue(':username', $username);
+        $stmt->bindValue(':useremail', $useremail);
+        $stmt->bindValue(':userpass', $userpassword);
+
+        $result = $stmt->execute();
+        if (isset($result)) {
+            $successMsg = 'Data inserted successfully';
+        }
+    }
+    /* After adding new data , we need to refresh the data table */
+    $resultall = showAllData($pdo);
+}
+
+echo '</pre>';
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Form</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+</head>
+
+<body>
+    <div class="container border">
+        <div class="row">
+            <div class="col">
+                <h1>This is a Registration form</h1>
+                <h3 class="text-danger">
+                    <?php
+                    if (isset($successMsg) && !empty($successMsg)) {
+                        echo $successMsg;
+                    }
+                    if (isset($errorStr) && strlen($errorStr) > 0) {
+                        echo $errorStr;
+                    }
+                    ?></h3>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col">
+                <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post" onsubmit="return CheckFormValidation()" name="registrationform" enctype="">
+                    <div class="form-group">
+                        <label for="">User Name</label>
+                        <input type="text" name="username" id="username" class="form-control" placeholder="Your name please" aria-describedby="helpId" required>
+                        <small id="helpId" class="text-muted">Help text</small>
+                    </div>
+                    <div class="form-group">
+                        <label for="useremail">Email id</label>
+                        <input type="email" name="useremail" id="useremail" class="form-control" placeholder="Your valid email address" aria-describedby="helpId" required>
+                        <small id="helpId" class="text-muted">Help text</small>
+                    </div>
+                    <div class="form-group">
+                        <label for="userpassword">Password</label>
+                        <input type="password" name="userpassword" id="userpassword" class="form-control" placeholder="Must be minimal 6 Character long & max 10" aria-describedby="helpId" maxlength="10" minlength="6" required>
+                        <small id="helpId" class="text-muted">Help text</small>
+                    </div>
+                    <div class="form-group">
+                        <label for="confirmpassword">Confirm Password</label>
+                        <input type="password" name="confirmpassword" id="confirmpassword" class="form-control" placeholder="Confirm your password" aria-describedby="confirmPasswordhelpId" maxlength="10" minlength="6" required>
+                        <small id="confirmPasswordhelpId" class="text-muted">Help text</small>
+                    </div>
+                    <div class="form-group">
+                        <button type="submit" class="btn btn-primary" name="submitformbtn">Submit</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+        <div class="row">
+            <?php if (isset($resultall) && ($resultall->rowCount()) > 0) { ?>
+                <table class="table">
+                    <thead>
+                        <tr>
+                            <th>Name</th>
+                            <th>Email</th>
+                            <th>Password</th>
+                            <th>Edit</th>
+                            <th>Delete</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php
+                        foreach ($resultall->fetchAll() as $key => $value) {
+                            echo "<tr>";
+                            echo "<td scope='row'>" . $value['username'] . "</td>";
+                            echo "<td>" . $value['useremail'] . "</td>";
+                            echo "<td>" . $value['userpass'] . "</td>";
+                            echo "<td><a href='" . $_SERVER['PHP_SELF'] . "?edituser=" . $value['id'] . "'>Edit</a></td>";
+                            echo "<td><a href='?deleteuser=" . $value['id'] . "'>Delete</a></td>";
+                            echo "</tr>";
+                        }
+                        ?>
+                    </tbody>
+                </table>
+            <?php } ?>
+        </div>
+    </div>
+
+    <script>
+        console.log('Hello world');
+        let CheckFormValidation = function() {
+            console.log('form is Submitted');
+            let passValue = document.getElementById('userpassword').value;
+            console.log(passValue);
+            let confirmPassValue = document.getElementById('confirmpassword').value;
+            console.log(confirmPassValue);
+            if (passValue !== confirmPassValue) {
+                console.log('Password Not match!');
+                let errorPass = document.getElementById('confirmPasswordhelpId');
+                errorPass.innerHTML = 'Password not matched!';
+                errorPass.classList.remove('text-muted');
+                errorPass.classList.add('text-danger');
+                return false;
+            }
+            return true;
+        }
+    </script>
+
+
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous">
+    </script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.7/dist/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous">
+    </script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous">
+    </script>
+</body>
+
+</html>
