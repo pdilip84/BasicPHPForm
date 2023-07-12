520415ab0b0177d0f4ee06b28139b666ed0765fd
pdilip84 <parmardilip2013@gmail.com>
2023-07-12 12:52:43 +0530

    Delete & update are working in data table

--- index.php
+++ index.php
@@ -20,7 +20,13 @@ $errorAry = array();
 if (isset($_REQUEST['edituser']) && !empty($_REQUEST['edituser'])) {
     $edituser =  $_REQUEST['edituser'];
     // echo $edituser;
-    editUser($edituser);
+    $userData = editUser($edituser, $pdo);
+    $edituid = $userData[0];
+    $edituname = $userData[1];
+    $edituemail = $userData[2];
+    // echo $edituid;
+    // echo $edituname;
+    // echo $edituemail;
 }
 
 /* If request for delete user */
@@ -40,12 +46,37 @@ $resultall = showAllData($pdo);
 // echo $resultall->rowCount();
 $resultall->setFetchMode(PDO::FETCH_ASSOC);
 
-function editUser($id)
+function editUser($id, $pdo)
 {
-    echo $id;
+    return $userData = selectSingleUser($id, $pdo);
 }
+
+function selectSingleUser($id, $pdo)
+{
+    // SELECT * FROM `users` WHERE `id` = 50
+    $stmt = $pdo->prepare("SELECT * FROM `users` WHERE `id` = :id");
+    $stmt->bindValue(':id', $id);
+    $stmt->execute();
+    foreach ($result = $stmt->fetchall(PDO::FETCH_ASSOC) as $key => $value) {
+        // print_r($value);
+        $editUid = $value['id'];
+        $editUname = $value['username'];
+        $editUemail = $value['useremail'];
+        $editUpassword = $value['userpass'];
+        return array($editUid, $editUname, $editUemail, $editUpassword);
+    }
+}
+
+// function updateform($editUid, $editUemail, $editUpassword)
+// {
+//     $updateForm = 1;
+//     // echo $editUemail;
+//     // echo $editUid;
+//     // echo $editUpassword;
+// }
 function deleteUser($id, $pdo)
 {
+
     // "DELETE FROM users WHERE `users`.`id` = 27"
     $stmt = $pdo->prepare("DELETE FROM users WHERE `users`.`id` = :id");
     // $pdo->bindValue(':id', $id);
@@ -75,6 +106,22 @@ function insertNewUser($pdo, $username, $useremail, $userpassword)
         $successMsg = 'Data is not inserted, Try again!';
     }
     return $successMsg;
+}
+
+function updateUser($pdo, $userid, $username, $useremail, $userpassword)
+{
+    // echo 'updating user now' . $userid;
+    // UPDATE `users` SET `username` = 'DilipDone11', `useremail` = 'demo@demo.in11', `userpass` = '$2y$10$Z2r3QL3vpllTz1uQI90KZuUrEVAdv.EsYc5OHuw1QWPajbVagxCLS22211' WHERE `users`.`id` = 70;
+    $stmt = $pdo->prepare("UPDATE `users` SET `id` = :id, `username` = :username, `useremail` = :useremail, `userpass` = :userpass, `inserdata` = now() WHERE `users`.`id` = :id;");
+    $stmt->bindValue(':id', $userid);
+    $stmt->bindValue(':username', $username);
+    $stmt->bindValue(':useremail', $useremail);
+    $stmt->bindValue(':userpass', $userpassword);
+    $editresult = $stmt->execute();
+    if (isset($editresult)) {
+        $successMsg = 'Data Updated successfully';
+        return $successMsg;
+    }
 }
 
 /* When form is submitting */
@@ -127,7 +174,11 @@ if (isset($_POST['submitformbtn'])) {
         // echo $userpassword;
         // echo password_verify($userpassword, '$2y$10$J9YPmAqYp.7yqfGIEhhEtuKDEEEyH01ARWoB0yScLEM6eAmc11OhW');
 
-        $successMsg = insertNewUser($pdo, $username, $useremail, $userpassword);
+        if (isset($_REQUEST['edituser'])) {
+            $successMsg = updateUser($pdo, $_REQUEST['edituser'], $username, $useremail, $userpassword);
+        } else {
+            $successMsg = insertNewUser($pdo, $username, $useremail, $userpassword);
+        }
     }
     /* After adding new data , we need to refresh the data table */
     $resultall = showAllData($pdo);
@@ -141,8 +192,9 @@ echo '</pre>';
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>PHP Form</title>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <title>PHP Form-dilip</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css"
+        integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 </head>
 
 <body>
@@ -152,6 +204,7 @@ echo '</pre>';
                 <h1><a href="<?php echo $_SERVER['PHP_SELF']; ?>">Registration form</a></h1>
                 <h3 class="text-danger">
                     <?php
+
                     if (isset($successMsg) && !empty($successMsg)) {
                         echo $successMsg;
                     }
@@ -163,25 +216,36 @@ echo '</pre>';
         </div>
         <div class="row">
             <div class="col">
-                <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post" onsubmit="return CheckFormValidation()" name="registrationform" enctype="">
+                <form action="<?php echo $_SERVER['REQUEST_URI']; ?>" method="post"
+                    onsubmit="return CheckFormValidation()" name="registrationform" enctype="">
                     <div class="form-group">
                         <label for="">User Name</label>
-                        <input type="text" name="username" id="username" class="form-control" placeholder="Your name please" aria-describedby="helpId" required>
+                        <input type="text" name="username" id="username" value="<?php if (isset($edituname)) {
+                                                                                    echo $edituname;
+                                                                                } ?>" class="form-control"
+                            placeholder="Your name please" aria-describedby="helpId" required>
                         <small id="helpId" class="text-muted">Help text</small>
                     </div>
                     <div class="form-group">
                         <label for="useremail">Email id</label>
-                        <input type="email" name="useremail" id="useremail" class="form-control" placeholder="Your valid email address" aria-describedby="helpId" required>
+                        <input type="email" name="useremail" id="useremail" value="<?php if (isset($edituemail)) {
+                                                                                        echo $edituemail;
+                                                                                    } ?>" class="form-control"
+                            placeholder="Your valid email address" aria-describedby="helpId" required>
                         <small id="helpId" class="text-muted">Help text</small>
                     </div>
                     <div class="form-group">
                         <label for="userpassword">Password</label>
-                        <input type="password" name="userpassword" id="userpassword" class="form-control" placeholder="Must be minimal 6 Character long & max 10" aria-describedby="helpId" maxlength="10" minlength="6" required>
+                        <input type="password" name="userpassword" id="userpassword" class="form-control"
+                            placeholder="Must be minimal 6 Character long & max 10" aria-describedby="helpId"
+                            maxlength="10" minlength="6" required>
                         <small id="helpId" class="text-muted">Help text</small>
                     </div>
                     <div class="form-group">
                         <label for="confirmpassword">Confirm Password</label>
-                        <input type="password" name="confirmpassword" id="confirmpassword" class="form-control" placeholder="Confirm your password" aria-describedby="confirmPasswordhelpId" maxlength="10" minlength="6" required>
+                        <input type="password" name="confirmpassword" id="confirmpassword" class="form-control"
+                            placeholder="Confirm your password" aria-describedby="confirmPasswordhelpId" maxlength="10"
+                            minlength="6" required>
                         <small id="confirmPasswordhelpId" class="text-muted">Help text</small>
                     </div>
                     <div class="form-group">
@@ -192,18 +256,18 @@ echo '</pre>';
         </div>
         <div class="row">
             <?php if (isset($resultall) && ($resultall->rowCount()) > 0) { ?>
-                <table class="table">
-                    <thead>
-                        <tr>
-                            <th>Name</th>
-                            <th>Email</th>
-                            <th>Password</th>
-                            <th>Edit</th>
-                            <th>Delete</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <?php
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th>Name</th>
+                        <th>Email</th>
+                        <th>Password</th>
+                        <th>Edit</th>
+                        <th>Delete</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php
                         foreach ($resultall->fetchAll() as $key => $value) {
                             echo "<tr>";
                             echo "<td scope='row'>" . $value['username'] . "</td>";
@@ -214,38 +278,41 @@ echo '</pre>';
                             echo "</tr>";
                         }
                         ?>
-                    </tbody>
-                </table>
+                </tbody>
+            </table>
             <?php } ?>
         </div>
     </div>
 
     <script>
-        console.log('Hello world');
-        let CheckFormValidation = function() {
-            console.log('form is Submitted');
-            let passValue = document.getElementById('userpassword').value;
-            console.log(passValue);
-            let confirmPassValue = document.getElementById('confirmpassword').value;
-            console.log(confirmPassValue);
-            if (passValue !== confirmPassValue) {
-                console.log('Password Not match!');
-                let errorPass = document.getElementById('confirmPasswordhelpId');
-                errorPass.innerHTML = 'Password not matched!';
-                errorPass.classList.remove('text-muted');
-                errorPass.classList.add('text-danger');
-                return false;
-            }
-            return true;
+    console.log('Hello world');
+    let CheckFormValidation = function() {
+        console.log('form is Submitted');
+        let passValue = document.getElementById('userpassword').value;
+        console.log(passValue);
+        let confirmPassValue = document.getElementById('confirmpassword').value;
+        console.log(confirmPassValue);
+        if (passValue !== confirmPassValue) {
+            console.log('Password Not match!');
+            let errorPass = document.getElementById('confirmPasswordhelpId');
+            errorPass.innerHTML = 'Password not matched!';
+            errorPass.classList.remove('text-muted');
+            errorPass.classList.add('text-danger');
+            return false;
         }
+        return true;
+    }
     </script>
 
 
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+        integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous">
     </script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.7/dist/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.7/dist/umd/popper.min.js"
+        integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous">
     </script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/js/bootstrap.min.js"
+        integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous">
     </script>
 </body>
 
